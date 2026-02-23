1e5da804b65f1a6e42a761354f3a6ec70e0515a9
srmp47 <srmpoor47@gmail.com>
2026-02-23 15:11:40 +0330

    Fullstack: <fix> fix errors in 2 files.

--- Backend/ProjBackend/accounts/views.py
+++ Backend/ProjBackend/accounts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, status, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, status, viewsets, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
